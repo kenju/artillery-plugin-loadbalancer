104f9c65cb41859cd46672158edffd8e8c6c8c8e
kenju <ken901waga@gmail.com>
2020-03-01 22:27:28 +0900

    support roundrobin

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const debug = require('debug')('plugin:loadbalancer');
+
 module.exports = {
   Plugin
 };
@@ -6,7 +8,53 @@ function Plugin(script, ee) {
   this.script = script;
   this.ee = ee;
 
-  console.log(this.script)
+  const { loadbalancer: config } = this.script.config.plugins
 
+  if (!script.config.processor) {
+    script.config.processor = {};
+  }
+
+  script.scenarios.forEach(function (scenario) {
+    scenario.beforeRequest = [].concat(scenario.beforeRequest || []);
+    scenario.beforeRequest.push('loadBalance');
+  });
+
+  script.config.processor.loadBalance = loadBalance(config);
+
+  debug('plugin initialized');
+  debug(config)
   return this;
 };
+
+function* roundRobin(config) {
+  const { targets } = config
+  let idx = 0
+
+  while (true) {
+    const { target } = targets[idx++]
+
+    // when reaches to the end, set index to the first item
+    if (idx == (targets.length)) {
+      idx = 0
+    }
+
+    yield target
+  }
+}
+
+// TODO: support weighted roundrobin
+function loadBalance(config) {
+  const { strategy } = config
+
+  if (strategy !== 'roundrobin') {
+    throw new Error(`strategy=${strategy} is not supported.`)
+  }
+
+  const iterator = roundRobin(config)
+
+  return (context) => {
+    const nextTarget = iterator.next().value
+    debug(`nextTarget=${nextTarget}`)
+    context.vars.target = nextTarget
+  }
+}
